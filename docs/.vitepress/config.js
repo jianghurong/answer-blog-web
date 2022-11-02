a4119f0bd6ffbee1f28476bcc89bab1d5665268b
jianghuorng <1164310634@qq.com>
2022-11-02 20:23:35 +0800

    feat: 更新

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,16 +1,16 @@
 export default {
-  base: '/answer-blog-web/',
-	title: '答案博客 | 答案如刀',
-	description: '秣马厉兵，砥砺前行。',
-  themeConfig: {
-    siteTitle: 'AnswerBlog',
-    sidebar: [
-      {
-        text: 'Nuxt',
-        items: [
-          { text: 'Introduction', link: '/nuxt/create'}
+    base: '/answer-blog-web/',
+    title: '答案博客 | 答案如刀',
+    description: '秣马厉兵，砥砺前行。',
+    themeConfig: {
+        siteTitle: 'AnswerBlog',
+        sidebar: [
+            {
+                text: 'Nuxt',
+                items: [
+                    { text: 'Introduction', link: '/nuxt/create' }
+                ]
+            }
         ]
-      }
-    ]
-  }
+    }
 }
