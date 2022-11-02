3bc9e3b931d0d9c9e8b12a95e9b5672d7c52da9d
荣易 <jianghr@mobduos.com>
2022-11-02 18:29:36 +0800

    feat: config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,15 +2,15 @@ export default {
   base: '/answer-blog-web/',
 	title: '答案博客 | 答案如刀',
 	description: '秣马厉兵，砥砺前行。',
-  themeConfig: {
-    siteTitle: 'AnswerBlog',
-    sidebar: [
-      {
-        text: 'Nuxt',
-        items: [
-          { text: 'Introduction', link: '/nuxt/create'}
-        ]
-      }
-    ]
-  }
+	themeConfig: {
+		siteTitle: 'AnswerBlog',
+		sidebar: [
+		{
+			text: 'Nuxt',
+			items: [
+			{ text: 'Introduction', link: '/nuxt/create'}
+			]
+		}
+		]
+	}
 }
