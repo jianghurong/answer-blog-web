490102886134bced8ab0240ceb965cb394a78a18
jianghurong <jianghurong@users.noreply.github.com>
2022-11-03 03:54:37 +0000

    deploy: 910e505dee7de8b6b9c8ec82a35a37e5ac72c86c

---
+++ assets/es6_index.md.b090d2b5.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.d5bc0a83.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"es6/index.md"}'),n={name:"es6/index.md"};function r(s,o,c,d,i,p){return a(),t("div")}const f=e(n,[["render",r]]);export{m as __pageData,f as default};
